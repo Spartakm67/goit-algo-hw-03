7940a7363625cd3b81b892a1e51063198eae16a0
Spartak <109909363+Spartakm67@users.noreply.github.com>
2026-04-14 12:07:44 +0300

    Start task-1

--- task-1.py
+++ task-1.py
@@ -1,6 +1,23 @@
 from datetime import datetime
 
+def string_to_date(date_string):
+    return datetime.strptime(date_string, "%Y-%m-%d").date()
+
+def get_days_from_today(date):
+
+    today =  datetime.today().date()
+    print(today)
+
+    given_data = string_to_date(date)
+
+    delta_days = (today - given_data).days
+    print(delta_days)
+    
+    return delta_days
 
 
-today =  datetime.today().date()
-print(today)
+
+get_days_from_today("2021-10-09")
+
+
+
